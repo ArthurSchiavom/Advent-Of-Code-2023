ca12f5dc9561974fca9f685de8fc1a695fc195e0
Arthur Schiavom <67876411+ArthurSchiavom@users.noreply.github.com>
2023-12-06 23:16:25 +0000

    part2 with bigdecimal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.math.BigDecimal.ONE;
+import static java.math.BigDecimal.ZERO;
 
 /**
  * Solution for the advent of code 2023 6
@@ -17,63 +23,69 @@ public class Main {
                 new RaceResult(80, 1491)
         );
 
-        final List<Long> marginOfError = new ArrayList<>();
+        final List<BigDecimal> marginOfError = new ArrayList<>();
         for (final RaceResult raceResult : raceResults) {
             marginOfError.add(calculateMarginOfError(raceResult));
         }
 
-        final long totalPossibilities = marginOfError.stream().reduce(1L, (result, nextMargin) -> result * nextMargin);
+        final BigDecimal totalPossibilities = marginOfError.stream().reduce(ONE, (result, nextMargin) -> result.multiply(nextMargin));
         System.out.println("PART 1 - Total number of possibilities to win: " + totalPossibilities);
 
 
-        System.out.println("PART 2 - Total number of possibilities to win: " + calculateMarginOfError(new RaceResult(4477080L, 283113411341491L)));
+        System.out.println("PART 2 - Total number of possibilities to win: " + calculateMarginOfError(new RaceResult(44707080L,
+                283113411341491L)));
+//                         10022122663200
+//                         1252765332900
     }
 
-    private static Long calculateMarginOfError(final RaceResult raceResult) {
-        final long minimumHoldTime = calculateMinButtonHoldTime(raceResult.getDuration(), raceResult.getWinDistance());
+    private static BigDecimal calculateMarginOfError(final RaceResult raceResult) {
+        final BigDecimal minimumHoldTime = calculateMinButtonHoldTime(new BigDecimal(raceResult.getDuration()), new BigDecimal(raceResult.getWinDistance()));
 
         // The valid values are those between
-        return raceResult.getDuration() - ((minimumHoldTime - 1) * 2) - 1;
+        return new BigDecimal(raceResult.getDuration()).subtract(minimumHoldTime.subtract(ONE).multiply(TWO)).subtract(ONE);
     }
 
-    private static long calculateMinButtonHoldTime(final long raceDuration, final long winDistance) {
-        long currentTestValue = (long)Math.ceil(raceDuration / 2.0D);
-        long currentMaxValue = raceDuration;
-        long currentMinValue = 0;
-        long isMinimumButtonHoldTime = 1;
+    private static final BigDecimal TWO = new BigDecimal(2);
+    private static final BigDecimal MINUS_ONE = new BigDecimal(-1);
+    private static BigDecimal calculateMinButtonHoldTime(final BigDecimal raceDuration, final BigDecimal winDistance) {
+        BigDecimal currentTestValue = raceDuration.divide(TWO, RoundingMode.CEILING);
+        BigDecimal currentMaxValue = raceDuration;
+        BigDecimal currentMinValue = ZERO;
+        int isMinimumButtonHoldTime = 1;
         while (isMinimumButtonHoldTime != 0) {
+            System.out.println("Testing " + currentTestValue);
             if (isMinimumButtonHoldTime == -1) {
                 currentMinValue = currentTestValue;
-                currentTestValue = currentTestValue + (int)(Math.ceil((currentMaxValue - currentTestValue) / 2.0D));
+                currentTestValue = currentTestValue.add(currentMaxValue.subtract(currentTestValue).divide(TWO, RoundingMode.CEILING));
             }
             else if (isMinimumButtonHoldTime == 1) {
                 currentMaxValue = currentTestValue;
-                currentTestValue = currentTestValue - (int)(Math.ceil((currentTestValue - currentMinValue) / 2.0D));
+                currentTestValue = currentTestValue.subtract(currentTestValue.subtract(currentMinValue).divide(TWO, RoundingMode.CEILING));
             }
             else {
                 break;
             }
             isMinimumButtonHoldTime = isMinimumButtonHoldTime(raceDuration, currentTestValue, winDistance);
         }
-        System.out.println("intermediary result " + currentTestValue);
+//        System.out.println("intermediary result " + currentTestValue);
         return currentTestValue;
     }
 
-    private static long isMinimumButtonHoldTime(final long raceDuration, final long buttonHoldTime, final long winDistance) {
-        final long finalDistance = distanceTraveled(raceDuration, buttonHoldTime);
-        if (finalDistance <= winDistance) {
+    private static int isMinimumButtonHoldTime(final BigDecimal raceDuration, final BigDecimal buttonHoldTime, final BigDecimal winDistance) {
+        final BigDecimal finalDistance = distanceTraveled(raceDuration, buttonHoldTime);
+        if (finalDistance.compareTo(winDistance) <= 0) {
             return -1;
         }
 
-        final long previousValueDistance = distanceTraveled(raceDuration, buttonHoldTime - 1);
-        if (previousValueDistance > winDistance) {
+        final BigDecimal previousValueDistance = distanceTraveled(raceDuration, buttonHoldTime.subtract(ONE));
+        if (previousValueDistance.compareTo(winDistance) > 0) {
             return 1;
         }
         return 0;
     }
 
-    private static long distanceTraveled(long raceDuration, long buttonHoldTime) {
-        return buttonHoldTime * (raceDuration - buttonHoldTime);
+    private static BigDecimal distanceTraveled(BigDecimal raceDuration, BigDecimal buttonHoldTime) {
+        return buttonHoldTime.multiply(raceDuration.subtract(buttonHoldTime));
     }
 
 
